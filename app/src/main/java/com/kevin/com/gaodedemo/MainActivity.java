9b01e6204c6e10fb374f992ab177c9c07be56a55
liuyang2 <liuyang3565@126.com>
2016-07-20 16:36:16 +0800

    16:36

--- app/src/main/java/com/kevin/com/gaodedemo/MainActivity.java
+++ app/src/main/java/com/kevin/com/gaodedemo/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //text
+        //asdadasd
+        //text 16:36
     }
 }
